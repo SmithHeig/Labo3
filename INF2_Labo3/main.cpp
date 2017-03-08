4d7b397535aac6957d0fa3c35fda5bd6cc4f5662
Smithja <james.smith@heig-vd.ch>
2017-03-08 13:35:50 +0100

    First commit

---
+++ INF2_Labo3/main.cpp
@@ -0,0 +1,25 @@
+/*
+-----------------------------------------------------------------------------------
+Laboratoire : <nn>
+Fichier     : <nom du fichier>.cpp
+Auteur(s)   : James Smith
+Date        : <jj.mm.aaaa>
+
+But         : <à compléter>
+
+Remarque(s) : <à compléter>
+
+Compilateur : MinGW-g++ 5.3.0
+-----------------------------------------------------------------------------------
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+   return 0;
+}
+
